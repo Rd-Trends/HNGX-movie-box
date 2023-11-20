db19e75f4e0887ca01125fb18ce363232042bd12
Daniel Ikoyo <dannirolands@gmail.com>
2023-11-20 16:44:25 +0100

    check test

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -6,28 +6,27 @@ describe("home page functions as it should", () => {
     cy.get('[data-testid="search-result"]').should("not.exist");
     cy.get('[data-testid="search-input"]')
       .should("be.visible")
-      .eq(0)
-      .type("The Matrix")
-      .wait(1000);
-    cy.get('[data-testid="search-result"]')
-      .should("exist")
-      .within(() => {
-        cy.wait(1000);
-        cy.get("a").should("not.have.length", 0).first().click();
-      });
+      .first()
+      .type("The Matrix");
+    // cy.get('[data-testid="search-result"]')
+    //   .should("be.visible")
+    //   .first()
+    //   .within(() => {
+    //     cy.wait(1000);
+    //     cy.get("a").should("not.have.length", 0).first().click();
+    //   });
   });
 
   it("users can see top 10 movie", () => {
     cy.contains(/Featured Movies/i);
     cy.get('[data-testid="movie-card"]')
       .should("have.length", 10)
-      .each(() => {
-        
-      })
-      .within(() => {
-        cy.get('[data-testid="movie-poster"]').should("be.visible");
-        cy.get('[data-testid="movie-title"]').should("be.visible");
-        cy.get('[data-testid="movie-release-date"]').should("be.visible");
+      .each((el) => {
+        cy.wrap(el).within(() => {
+          cy.get('[data-testid="movie-poster"]').should("be.visible");
+          cy.get('[data-testid="movie-title"]').should("be.visible");
+          cy.get('[data-testid="movie-release-date"]').should("be.visible");
+        });
       });
   });
 });
